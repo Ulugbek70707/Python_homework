c836dddb7c5f54b79f2670662c087eeaa5dc8870
Ulug'bek Xudoyberdiyev <ulugbek.khudoyberdiyev@gmail.com>
2025-07-12 15:53:42 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,35 @@
+# 1
+side = float(input("Enter the length of the side of the square: "))
+perimeter = 4*side
+area = side*side
+
+print("Perimeter of the square is:", perimeter)
+print("Area of the square is:", area)
+
+# 2
+diameter = float(input("Enter diameter of circle: "))
+length = 3.1415*diameter
+
+print("Length of circle is:", length)
+
+# 3
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+mean = (a+b)/2
+
+print("The mean of two numbers is:", mean)
+
+# 4
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+Sum = a+b
+Product = a*b
+Square_a = a**2
+Square_b = b**2
+
+print("The sum of two numbers is:", Sum)
+print("The product of two numbers is:", Product)
+print("The square of a:", Square_a)
+print("The square of b:", Square_b)
